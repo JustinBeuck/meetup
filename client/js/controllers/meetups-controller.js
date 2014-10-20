b067cdd3c21c14c4a82e25d0ac41c9e5790cb00b
Justin Beuck <jbeuck@yahoo.com>
2014-10-19 21:22:19 -0700

    completed tutorial

--- client/js/controllers/meetups-controller.js
+++ client/js/controllers/meetups-controller.js
@@ -1,15 +1,18 @@
-app.controller('meetupsController', ['$scope', '$resource', 
-	function ($scope, $resource) {
-		var Meetup = $resource('/api/meetups');
-		$scope.meetups = [
-			{ name: "MEAN SF Developers"},
-			{ name: "MEAN Austin Developers" }
-		]
+app.controller('meetupsController', ['$scope', '$resource', function ($scope, $resource) {
+  var Meetup = $resource('/api/meetups');
 
-		$scope.createMeetup = function () {
-			var meetup = new Meetup();
-			meetup.name = $scope.meetupName;
-			meetup.$save();
-		}
+  Meetup.query(function (results) {
+    $scope.meetups = results;
+  });
 
-	}])
+  $scope.meetups = []
+
+  $scope.createMeetup = function () {
+    var meetup = new Meetup();
+    meetup.name = $scope.meetupName;
+    meetup.$save(function (result) {
+      $scope.meetups.push(result);
+      $scope.meetupName = '';
+    });
+  }
+}]);
